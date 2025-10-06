93e8937a244e22d50854a36347f59ef75d789c2a
asifsarc <asifulislamweb2.0@gmail.com>
2025-10-07 02:08:47 +0600

    Redefine User Routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { StudentsRoutes } from './app/modules/student/student.route';
-import { UsersRoutes } from './app/modules/user/user.route';
+
 const app: Application = express();
 
 // parser
@@ -12,7 +12,7 @@ app.use(cors());
 // application routes
 
 app.use('/api/v1/students', StudentsRoutes);
-app.use('/api/v1/users', UsersRoutes);
+
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
